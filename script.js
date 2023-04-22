4dab3ee8ce1e89c964f23780949234bbc6edb09d
NPO <npofred45@gmail.com>
2023-04-22 09:54:36 -0400

    basic messaging service

--- script.js
+++ script.js
@@ -1,16 +1,36 @@
 
 var peer = new Peer();
-var conn;
+var connin = null;
+var connout = null
 var newID;
+var stdout = document.getElementById("demo");
+var Status = document.getElementById("status");
 peer.on('open', function(id) {
     newID = id;
     var x = document.createTextNode('My peer ID is: '+ newID);
     document.body.appendChild(x);
 });
-function myFunction() {
+function TryConnect() {
     var connectID = document.getElementById("myText").value;
-    conn = peer.connect(connectID);
+    connout = peer.connect(connectID);
 }
-peer.on('connection',function(conn){
-    document.getElementById("demo").innerHTML = 'Connected to:'+conn.peer;
+function SendHello(){
+    if (connout){
+        console.log("sending");
+        connout.send(document.getElementById("myText").value);
+    }
+}
+
+
+peer.on('connection',function(connin){
+    console.log("on connection");
+    Status.innerHTML = "Connected to: "+connin.peer
 })
+peer.on('connection', function (c) {
+    connin = c;
+    console.log("Connected to: " + connin.peer);
+    connin.on('data', function (data){
+        console.log("data recived",data);
+        stdout.innerHTML = "data recived: "+data;
+    });
+});
